c6266a2c91f6d39972da43574d2ed1c386b238a2
AryanKeni05 <aryankeni05@gmail.com>
2026-03-20 22:36:52 +0530

    Added requirements.txt

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,7 @@
 import mysql.connector
 
+import sqlite3
+
 def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
@@ -7,3 +9,9 @@ def get_db_connection():
         password="root",   # your password
         database="dryfruit_store"
     )
+
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
